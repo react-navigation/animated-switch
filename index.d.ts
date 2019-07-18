34eb37fe6b192bb925a967271cbb0e31db994196
Sunny Luo <sunnylqm@qq.com>
2019-07-18 18:26:21 +0800

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -3,7 +3,7 @@ import { NavigationRouteConfigMap, SwitchNavigatorConfig, NavigationContainer } 
 import { Transition } from 'react-native-reanimated';
 
 export interface AnimatedSwitchNavigatorConfig extends SwitchNavigatorConfig {
-  transition?: React.Element<typeof Transition>;
+  transition?: React.ReactElement<typeof Transition>;
 }
 
 export default function createAnimatedSwitchNavigator(
